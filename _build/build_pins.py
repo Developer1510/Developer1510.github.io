87dc6752a7f544aa6d2089c59065bffca1aef822
developer1510 <developer1510@sinergise.com>
2021-03-01 16:14:01 +0100

    test

--- _build/build_pins.py
+++ _build/build_pins.py
@@ -2,9 +2,11 @@ import os
 import json
 import glob
 import re
+import base64
 import markdown
 from PIL import Image
 
+eob_base_url = 'https://apps.sentinel-hub.com/eo-browser/?'
 max_pin_image_width = 640
 max_pin_image_height = 480
 
@@ -21,19 +23,38 @@ with open('_build/container.html', 'r', encoding='utf-8') as html_container_file
                             '\t\t\t\t\t<div id="pins">\n'
     
             for pins_json_file_name in glob.glob(os.path.join(pins_dir, '*.json')):
-                print("Pin file: " + pins_json_file_name)
                 with open(pins_json_file_name, 'r', encoding='utf-8') as pins_json_file:
                     pins = json.load(pins_json_file)
                     for pin in pins:
+                        is_location = 'lat' in pin and 'lng' in pin
+                        is_eob = is_location and 'zoom' in pin
+                        pin_lib_extra = pin.get('pinLibrary')
+                        thumbnail_path = pin_lib_extra.get('thumbnailPath') if pin_lib_extra else None 
+                        
+                        eob_url = eob_base_url
+                        for pin_key in pin.keys():
+                            if pin_key not in ('title', 'description', 'pinLibrary'):
+                                if pin_key == 'evalscript':
+                                    eob_url += '&' + pin_key + '=' + base64.b64encode(bytes(str(pin.get(pin_key)), 'utf-8')).decode("utf-8")
+                                else:
+                                    eob_url += '&' + pin_key + '=' + str(pin.get(pin_key))
+                          
+                        # print(eob_url)
+                        
+                        download_url = pin.get('visualizationUrl') or ''
+                
                         html_content += pin_html_template \
-                            .replace('{title}', pin['title']) \
-                            .replace('{date}', pin['toTime']) \
-                            .replace('{type}', pin['datasetId']) \
-                            .replace('{image_path}', pin['title']) \
-                            .replace('{world_pos_x}', str(int((pin['lng'] + 180) * 300 / 360))) \
-                            .replace('{world_pos_y}', str(int((-pin['lat'] + 90) * 150 / 180))) \
-                            .replace('{eob_url}}', pin['visualizationUrl']) \
-                            .replace('{download_url}', pin['visualizationUrl']) \
+                            .replace('{title}', pin.get('title') or '') \
+                            .replace('{date}', pin.get('toTime') or '') \
+                            .replace('{type}', pin.get('datasetId') or '') \
+                            .replace('{image_path}', thumbnail_path if thumbnail_path else '') \
+                            .replace('{world_pos_x}', str(int((pin['lng'] + 180) * 300 / 360)) if is_location else '0') \
+                            .replace('{world_pos_y}', str(int((-pin['lat'] + 90) * 150 / 180)) if is_location else '0') \
+                            .replace('{world_pos_display}', 'block' if is_location else 'none') \
+                            .replace('{eob_url}', eob_url) \
+                            .replace('{eob_display}', 'block' if is_eob else 'none') \
+                            .replace('{download_url}', download_url) \
+                            .replace('{download_display}', 'block' if download_url else 'none') \
                             .replace('{description}', markdown.markdown(pin['description']))
                 
             html_content += '\t\t\t\t\t</div>\n'
